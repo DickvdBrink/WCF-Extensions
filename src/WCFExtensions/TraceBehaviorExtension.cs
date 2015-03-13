f02a221db6da74fb966cd8b2d41a4b8f60ceba0d
Dick van den Brink <d_vandenbrink@outlook.com>
2015-03-13 20:43:33 +0100

    Added basic inspector stubs

---
+++ src/WCFExtensions/TraceBehaviorExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFExtensions
+{
+    public class TraceBehaviorExtension : BehaviorExtensionElement
+    {
+        public override Type BehaviorType
+        {
+            get
+            {
+                return typeof(TraceRequestInterceptor);
+            }
+        }
+
+        protected override object CreateBehavior()
+        {
+            return new TraceRequestInterceptor();
+        }
+    }
+}
---
+++ src/WCFExtensions/TraceRequestInterceptor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Dispatcher;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFExtensions
+{
+    public class TraceRequestInterceptor : IClientMessageInspector, IDispatchMessageInspector
+    {
+
+        #region IClientMessageInspector
+
+        public void AfterReceiveReply(ref Message reply, object correlationState)
+        {
+            System.ServiceModel.Description.IEndpointBehavior a;
+            throw new NotImplementedException();
+        }
+
+        public object BeforeSendRequest(ref Message request, IClientChannel channel)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IDispatchMessageInspector
+
+        public object AfterReceiveRequest(ref Message request, IClientChannel channel, InstanceContext instanceContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BeforeSendReply(ref Message reply, object correlationState)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
